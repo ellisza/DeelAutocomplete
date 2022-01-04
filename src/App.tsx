5018e36f3f9743fcbe971ac7a6b6f34bd6b1db0b
Zach Ellis <zacharysellis@gmail.com>
2022-01-04 13:04:27 -0700

    Autocomplete is functioning, a little ugly right now. Going to fix design as well set up asynchronous calls and this part of the take home task should be done.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import data from "./countries.json";
 import Autocomplete from "./components/autocomplete";
 
 function App() {
   return (
     <div className="App">
-      <Autocomplete word="hello" />
+      <Autocomplete data={data} />
     </div>
   );
 }
--- src/components/autocomplete.tsx
+++ src/components/autocomplete.tsx
@@ -1,11 +1,50 @@
-import React, { FC } from 'react';
+import React, { FC, ChangeEvent, useState } from 'react';
 import "../App.css";
 
 interface AutocompleteProps {
-  word: string;
+  data: ItemData[];
 }
 
-const Autocomplete: FC<AutocompleteProps> = ({ word }) => {
+interface ItemData {
+  name: string;
+  code: string;
+}
+
+const Autocomplete: FC<AutocompleteProps> = ({ data }) => {
+    const [search, setSearch] = useState({
+        text: "",
+        suggestions: [] as ItemData[]
+    });
+
+    const [isComponentVisible, setIsComponentVisible] = useState(true);
+
+    const onTextChanged = async (e: ChangeEvent<HTMLInputElement>) => {
+        const value = e.target.value;
+        let suggestions : ItemData[] = [];
+        try{
+            if (value.length > 0) {
+                const regex = new RegExp(`^${value}`, "i");
+                suggestions = data.sort().filter((v: ItemData) => regex.test(v.name));
+            }
+            setIsComponentVisible(true);
+            setSearch({ text: value, suggestions });
+        }
+        catch(error) {
+            console.error("Couldn't get data");
+        }
+    };
+
+    const suggestionSelected = (value: ItemData) => {
+        setIsComponentVisible(false);
+
+        setSearch({
+            text: value.name,
+            suggestions: []
+        });
+    };
+
+    const { suggestions } = search;
+
   return (
     <>
         <div className="Box">
@@ -13,8 +52,40 @@ const Autocomplete: FC<AutocompleteProps> = ({ word }) => {
                 <h2>Autocomplete Task</h2>
             </div>
             <div className="Instructions">
-                <span>Start typing to see it work:</span>
+                <span>This autocompletes country names:</span>
             </div>
+            
+            <div
+                onClick={() => setIsComponentVisible(false)}
+                style={{
+                display: isComponentVisible ? "block" : "none",
+                width: "200vw",
+                height: "200vh",
+                backgroundColor: "transparent",
+                position: "fixed",
+                zIndex: 0,
+                top: 0,
+                left: 0
+                }}
+            />
+            <div>
+                <input
+                id="input"
+                autoComplete="off"
+                value={search.text}
+                onChange={onTextChanged}
+                type={"text"}
+                />
+            </div>
+            {suggestions.length > 0 && isComponentVisible && (
+                <div className="AutocompleteList">
+                    {suggestions.map((item: ItemData) => (
+                        <span>
+                            {item.name}
+                        </span>
+                    ))}
+                </div>
+            )}
         </div>
     </>
   );
